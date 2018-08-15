6e0bdd461376de593129a4c3585e0c0f8db1c65b
Patrick Holmes <patrick.j.holmes@gmail.com>
2018-08-15 10:17:12 -0700

    Changes

---
+++ Prancercise Tracker Starter/DocumentDataStore.swift
@@ -0,0 +1,9 @@
+//
+//  HealthKitDocumentStore.swift
+//  Health Data Reader-Writer
+//
+//  Created by Patrick Holmes on 8/2/18.
+//  Copyright © 2018 Razeware LLC. All rights reserved.
+//
+
+import Foundation
